4f053ad800ac88b0a7841a8043ea23d90db0a323
AlainBel <albelspb@gmail.com>
2021-12-09 15:38:30 +0300

    Extract Before and After methods

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -16,18 +16,13 @@ public class ContactCreationTests {
     private StringBuffer verificationErrors = new StringBuffer();
     private JavascriptExecutor js;
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         wd = new ChromeDriver();
         baseUrl = "https://www.google.com/";
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         js = (JavascriptExecutor) wd;
-    }
-
-
-    @Test
-    public void testContactCreationTests() throws Exception {
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -37,6 +32,11 @@ public class ContactCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+
+    @Test
+    public void testContactCreation() throws Exception {
         wd.findElement(By.linkText("add new")).click();
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
@@ -55,10 +55,9 @@ public class ContactCreationTests {
         wd.findElement(By.name("theform")).click();
         wd.findElement(By.xpath("//input[21]")).click();
         wd.findElement(By.linkText("home page")).click();
-        wd.findElement(By.linkText("Logout")).click();
     }
 
-    @AfterClass(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         wd.quit();
         String verificationErrorString = verificationErrors.toString();
